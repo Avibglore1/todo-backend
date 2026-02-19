88187bd5fc2ccb34c8980a229838c9ec2e72817c
Avinash <avinashkumarofficial95@gmail.com>
2026-02-19 22:35:46 +0530

    feat: enhance todo functionality with CRUD operations and authentication middleware

--- controller/todo.controller.js
+++ controller/todo.controller.js
@@ -14,9 +14,47 @@ export const createToDo = async(req,res) =>{
 
 export const getToDos = async(req,res) =>{
     try {
-        const todos = await ToDo.find({user: req.user});
-        res.json(todos);
+        const todo = await ToDo.find({user: req.user});
+        res.json(todo);
     } catch (error) {
         res.status(500).json({error: error.message})
+    }
+}
+
+export const updateToDo = async(req,res) =>{
+    try {
+        const todo = await ToDo.findOneAndUpdate(
+            {_id:req.params.id, user:req.user.id},req.body,{new: true}
+        );
+        res.json(todo);
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+}
+
+export const deleteToDo = async(req,res) =>{
+    try {
+        await ToDo.findOneAndDelete({
+            _id:req.params.id,
+            user: req.user.id
+        });
+
+        res.json({message: "Todo deleted"});
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+};
+
+export const toggleToDo = async(req,res) =>{
+    try {
+        const todo = await ToDo.findOne({
+            _id: req.params.id,
+            user: req.user.id,
+        });
+
+        todo.completed = !todo.completed;
+        await todo.save()
+    } catch (error) {
+        return res.status(500).json({error: error.message})
     }
 }
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { createToDo,getToDos } from "../controller/todo.controller.js";
+import { createToDo,deleteToDo,getToDos, toggleToDo, updateToDo } from "../controller/todo.controller.js";
+import auth from "./../middleware/auth.js"
 
 const router = express.Router();
 
-router.post("/createToDo", createToDo);
-router.get("/getToDos", getToDos);
+router.post("/createToDo",auth, createToDo);
+router.get("/getToDos",auth, getToDos);
+router.put("/:id", auth, updateToDo);
+router.delete("/:id", auth, deleteToDo);
+router.patch("/:id/toggle", auth, toggleToDo);
 
 export default router
